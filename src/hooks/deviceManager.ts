2b0069efe8f5c2c852afe909bd650bcb93535d9e
wujing <jing.wu@seeed.cc>
2023-11-18 17:08:32 +0800

    feat: use enums instead of string

--- src/hooks/deviceManager.ts
+++ src/hooks/deviceManager.ts
@@ -1,6 +1,6 @@
 import { computed } from 'vue';
 import { Terminal } from 'xterm';
-import { deviceTypeObj } from '@/sscma/constants';
+import { DeviceType, deviceTypeObj } from '@/sscma/constants';
 import { useAppStore } from '@/store';
 import EspSerialDevice from '@/sscma/xiao_esp32s3/EspSerialDevice';
 import Himax from '@/sscma/vision_ai_we_II/deviceHimax';
@@ -9,8 +9,8 @@ function useDeviceManager() {
   const appStore = useAppStore();
   const device = computed(() => {
     const obj = {
-      [deviceTypeObj['XIAO ESP32S3'].name]: new EspSerialDevice(),
-      [deviceTypeObj['Vision AI (WE-II)'].name]: new Himax(),
+      [deviceTypeObj[DeviceType.XiaoEsp32s3].name]: new EspSerialDevice(),
+      [deviceTypeObj[DeviceType.VisionAIWeII].name]: new Himax(),
     };
     return obj[appStore.deviceType as string];
   });
--- src/pages/setup/process/index.vue
+++ src/pages/setup/process/index.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-  import { deviceTypeObj } from '@/sscma/constants';
+  import { DeviceType, deviceTypeObj } from '@/sscma/constants';
   import { useAppStore } from '@/store';
   import XiaoESP32S3Process from './xiao_esp32s3/index.vue';
   import VisionAIWeIIProcess from './vision_ai_we_II/index.vue';
@@ -9,10 +9,12 @@
 
 <template>
   <XiaoESP32S3Process
-    v-if="appStore.deviceType === deviceTypeObj['XIAO ESP32S3'].name"
+    v-if="appStore.deviceType === deviceTypeObj[DeviceType.XiaoEsp32s3].name"
   />
 
   <VisionAIWeIIProcess
-    v-else-if="appStore.deviceType === deviceTypeObj['Vision AI (WE-II)'].name"
+    v-else-if="
+      appStore.deviceType === deviceTypeObj[DeviceType.VisionAIWeII].name
+    "
   />
 </template>
