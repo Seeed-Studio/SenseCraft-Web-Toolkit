f3dbe2449eadea7edadae87c4a6181b822d5af33
LynnL4 <lht856@foxmail.com>
2023-11-29 15:33:14 +0800

    feat: set default address to 0x0000

--- src/pages/utils/vision_ai_we_II/index.vue
+++ src/pages/utils/vision_ai_we_II/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <Tools :flasher="flasher" />
+  <Tools :flasher="flasher" :read-file="readFile" />
 </template>
 
 <script lang="ts" setup>
@@ -7,4 +7,17 @@
   import Tools from '../components/Tools.vue';
 
   const flasher = new Flasher();
+
+  const readFile = (blob: Blob | File): Promise<Uint8Array> => {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        const arrayBuffer = reader.result as ArrayBuffer;
+        const uint8Array = new Uint8Array(arrayBuffer);
+        resolve(uint8Array);
+      };
+      reader.onerror = reject;
+      reader.readAsArrayBuffer(blob);
+    });
+  };
 </script>
