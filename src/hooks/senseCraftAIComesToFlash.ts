d068e815e1a88c5ca4ec567b7d86600d3c3a4762
吴敬悦 <wutiange20@qq.com>
2024-01-11 17:30:30 +0800

    feat: come to update uniform_type

--- src/hooks/senseCraftAIComesToFlash.ts
+++ src/hooks/senseCraftAIComesToFlash.ts
@@ -39,28 +39,18 @@ export async function fetchModelFileUrl(modelId: string, token: string) {
 const useSenseCraftAIComesToFlash = () => {
   const route = useRoute();
   const deviceStore = useDeviceStore();
-  const { id: modelId, token } = route.query ?? {};
+  const { id: modelId, token, uniform_type: uniformType } = route.query ?? {};
   const handleSenseCraftAI = async () => {
     if (!(modelId && token && route.name === 'process')) {
       return {};
     }
     const [
-      [
-        modelImg,
-        deviceTypes,
-        labels,
-        description,
-        size,
-        name,
-        aiFramework,
-        modelFormat,
-      ],
+      [modelImg, labels, description, size, name, aiFramework, modelFormat],
       modelFile,
       constant,
     ] = await Promise.all<any>([
       fetchModelDetail(modelId as string, [
         'pic_url',
-        'uniform_types',
         'labels',
         'description',
         'model_size',
@@ -71,42 +61,37 @@ const useSenseCraftAIComesToFlash = () => {
       fetchModelFileUrl(modelId as string, token as string),
       fetchConstant(),
     ]);
-    if (deviceTypes.length > 0) {
-      const deviceType: string = deviceTypes[0];
-      const deviceKeyToId: Record<string, string> = {
-        '32': DeviceType.XiaoEsp32s3,
-        '36': DeviceType.GroveAIWE2,
-      };
-      if (typeof deviceKeyToId[deviceType] === 'string') {
-        deviceStore.setDeviceTypeById(deviceKeyToId[deviceType]);
-      }
-      deviceStore.setComeToSenseCraftAI({
-        model: {
-          description,
-          classes:
-            labels?.reduce(
-              (
-                arr: string[],
-                e: { object_id: string; object_name: string }
-              ) => {
-                arr[Number(e.object_id)] = e.object_name;
-                return arr;
-              },
-              []
-            ) ?? [],
-          algorithm: constant.ai_framework_array[aiFramework as string],
-          name,
-          version: modelFile.version ?? '1.0.0',
-          category: modelFile.algorithm[0].algorithm_name ?? 'Object Detection',
-          model_type: constant.model_format_array[modelFormat],
-          size,
-          modelImg,
-          isCustom: true,
-        },
-        modelUrl: modelFile.arguments.url,
-      });
-      deviceStore.setFlashWay(FlashWayType.ComeToSenseCraftAI);
+    const deviceType: string = uniformType as string;
+    const deviceKeyToId: Record<string, string> = {
+      '32': DeviceType.XiaoEsp32s3,
+      '36': DeviceType.GroveAIWE2,
+    };
+    if (typeof deviceKeyToId[deviceType] === 'string') {
+      deviceStore.setDeviceTypeById(deviceKeyToId[deviceType]);
     }
+    deviceStore.setComeToSenseCraftAI({
+      model: {
+        description,
+        classes:
+          labels?.reduce(
+            (arr: string[], e: { object_id: string; object_name: string }) => {
+              arr[Number(e.object_id)] = e.object_name;
+              return arr;
+            },
+            []
+          ) ?? [],
+        algorithm: constant.ai_framework_array[aiFramework as string],
+        name,
+        version: modelFile.version ?? '1.0.0',
+        category: modelFile.algorithm[0].algorithm_name ?? 'Object Detection',
+        model_type: constant.model_format_array[modelFormat],
+        size,
+        modelImg,
+        isCustom: true,
+      },
+      modelUrl: modelFile.arguments.url,
+    });
+    deviceStore.setFlashWay(FlashWayType.ComeToSenseCraftAI);
     return null;
   };
   onMounted(async () => {
