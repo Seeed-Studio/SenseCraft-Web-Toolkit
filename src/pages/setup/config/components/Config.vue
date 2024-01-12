d46542090bf5fe80a5e785a8af6ba45c2e2f3327
吴敬悦 <wutiange20@qq.com>
2024-01-12 18:00:33 +0800

    fix: merge error

--- src/pages/setup/config/components/Config.vue
+++ src/pages/setup/config/components/Config.vue
@@ -80,7 +80,6 @@
           />
         </a-form-item>
         <a-form-item
-          v-if="!config.mqtt.ssl"
           field="mqtt.username"
           :label="$t('workplace.config.mqtt.username')"
         >
